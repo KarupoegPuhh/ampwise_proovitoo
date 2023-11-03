7dd7de70a1b388f9111d649fa254bddaf60e9a51
saamel <saamel.sonn@gmail.com>
2023-11-03 15:34:35 +0200

    hover

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { Label } from '../ui/label';
 import NumberField, { NumberFieldType } from './NumberField';
 import Selector from './Selector';
 import TextField from './TextField';
@@ -27,15 +29,30 @@ function Input(props: { label: string, type: InputType }) {
   }
 
   return (
-    <InputWrapper inputElement={inputElement}/>
+    <InputWrapper label={label} inputElement={inputElement}/>
   );
 }
 
-function InputWrapper(props: { inputElement: JSX.Element }) {
+function InputWrapper(props: { inputElement: JSX.Element, label: string }) {
+  const { label } = props;
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+    console.log("mouse in")
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
     <>
-      {props.inputElement}
-      Input Edit
+      <div className="flex items-stretch gap-4" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+        <Label className="font-bold float-left flex items-center justify-center" htmlFor={`id_${label}`}>{label}</Label>
+        {props.inputElement}
+        <p className={`flex items-center justify-center ${isHovering ? "visible" : "invisible"}`}>edit</p>
+      </div>
     </>
   );
 }
--- src/components/input/TextField.tsx
+++ src/components/input/TextField.tsx
@@ -1,16 +1,11 @@
 import { HTMLInputTypeAttribute } from 'react';
 import { Input } from "../ui/input";
-import { Label } from "../ui/label";
 
 function TextField(props: { label: string, inputType: HTMLInputTypeAttribute, placeholderText?: string }) {
   const { label, inputType, placeholderText } = props;
 
   return (
-    <div>
-      TextField
-      <Label htmlFor={`id_${label}`}>{label}</Label>
-      <Input type={inputType} id={`id_${label}`} placeholder={placeholderText}/>
-    </div>
+    <Input className="float-right" type={inputType} id={`id_${label}`} placeholder={placeholderText}/>
   );
 }
 
